53e171408ced51df51fb0611893cbff868e5eed0
Yann Jaulin <60672884+YannJaulin@users.noreply.github.com>
2021-02-08 15:36:17 +0000

    Complétion de l'ajout de Element

--- require/alert.php
+++ require/alert.php
@@ -2,9 +2,9 @@
         <div class="row">
             <div class="col-md">
                 <div class="alert alert-success" role="alert">
-                    <strong>03/02/2021 :</strong> Ça y est, un an après l'annonce, le nouveau site est en ligne, j'espère qu'il vous plaît et qu'il va vous aider à passer sur des alternatives qui respectent votre vie privée sur internet.
-                    Si vous souhaitez voir son code source et contribuer, il est disponible sur GitHub et il est sous la licence CC BY-SA 4.0 : <a href="https://github.com/YannJaulin/protege-moi-sur-internet" target="_blank" class="alert-link" style="text-decoration: underline;">https://github.com/YannJaulin/protege-moi-sur-internet</a>.
-                    N'hésitez pas à partager l'information autour de vous, afin de faire connaître l'annuaire, merci pour votre soutien et bonne navigation !
+                    <strong>08/02/2021 :</strong> Une nouvelle alternative a été ajoutée et il s'agit de Element, un logiciel de messagerie basé sur le protocole Matrix. Vous pouvez le consulter en allant sur Communication > Messagerie. Un grand merci aux personnes qui ont contribué à la création de sa présentation.
+                    Si vous souhaitez aider à l'évolution de l'annuaire et faire des suggestions, n'hésitez pas à le faire ici : <a href="https://github.com/YannJaulin/protege-moi-sur-internet" target="_blank" class="alert-link" style="text-decoration: underline;">https://github.com/YannJaulin/protege-moi-sur-internet</a>.
+                    Je termine en vous remerciant pour vos partages et vos messages, c'est vraiment gentil !
                 </div>
             </div>
         </div>
